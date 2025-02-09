0eef743bd0d48bcd1287f78026a140389b12bc19
Pubudu Sanka <pubudusanka79@gmail.com>
2025-02-09 11:10:16 +0530

    Update AttendanceService.java

--- backend/src/main/java/com/example/learning_management_system/service/AttendanceService.java
+++ backend/src/main/java/com/example/learning_management_system/service/AttendanceService.java
@@ -1,5 +1,9 @@
 package com.example.learning_management_system.service;
 
+import com.example.learning_management_system.dto.AttendanceDto;
+import com.example.learning_management_system.dto.AttendanceResponseDto;
+import com.example.learning_management_system.entity.Attendance;
+import com.example.learning_management_system.repository.AttendanceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
